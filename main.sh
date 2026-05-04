47b7e71add598e2c202abd05da128b3f94d6cfb4
Vincent Kmin <vini.ms01@outlook.com>
2026-05-05 00:09:58 +0200

    added comments

--- main.sh
+++ main.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# we need the username for the ssh link
 username=$(gh auth status -a --json hosts --jq '.hosts.[].[].login')
 read -p "Repository name: " repo_name
 
+# we need to check if the repo already exists and if not we create one with the choosen name
 if gh repo ls --json name --jq '.[].name' | grep -Fxq $repo_name; then
   :
 else
@@ -17,6 +19,7 @@ touch README.md .gitignore
 
 read -p $'\nChoose repo type(python/web/clear): ' repo_type
 if [ $repo_type == 'python' ]; then
+  # we create a python venv
   touch src/main.py
   python3 -m venv .venv
   source .venv/bin/activate
